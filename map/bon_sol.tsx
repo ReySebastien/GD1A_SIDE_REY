53aad48ac5a5315491853c489885adc7fb0ccc8f
ReySebastien <rey.babou35@free.fr>
2021-03-17 11:32:40 +0100

    test tiled

---
+++ map/bon_sol.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="bon_sol" tilewidth="32" tileheight="32" tilecount="180" columns="60">
+ <image source="../assets/bon_sol.png" width="1920" height="100"/>
+</tileset>
---
+++ map/fondespace.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="fondespace" tilewidth="32" tileheight="32" tilecount="5700" columns="100">
+ <image source="fondespace.png" width="3200" height="1824"/>
+</tileset>
